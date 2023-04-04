32411f1a998a52ef562767771dbdd87e6330775f
weiwenyu <2454150875@qq.com>
2023-04-04 23:32:51 +0800

    feat: init

---
+++ src/layout/header/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="layout-header">
+    <div class="left">
+      <span></span>
+    </div>
+    <div class="center"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout-header {
+  display: flex;
+
+  height: 40px;
+
+  border-bottom: 1px solid #bab8b8;
+
+  padding: 5px 30px;
+
+  .left {
+    width: 40%;
+
+    display: flex;
+    align-items: center;
+  }
+
+  .center {
+    width: 20%;
+  }
+
+  .right {
+    width: 40%;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+}
+</style>
--- src/views/module/home/index.vue
+++ src/views/module/home/index.vue
@@ -1,15 +1,13 @@
-<!--
- * @Author: weiwenyu 2454150875@qq.com
- * @Date: 2023-04-03 16:31:38
- * @LastEditors: weiwenyu 2454150875@qq.com
- * @LastEditTime: 2023-04-03 16:31:45
- * @FilePath: \new-video-websit\src\views\module\home\index.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LayoutHeader from "@/layout/header/index.vue";
+</script>
 
 <template>
-  <div>Home Page</div>
+  <div>
+    <!-- 顶部 -->
+    <LayoutHeader />
+    231321
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
